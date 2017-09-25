78c1450e889619f90482dcdb5081325253cbe392
Brusevolddalen <martin_brusevolddalen@hotmail.com>
2017-09-25 14:50:02 +0200

    lower case in path files

--- js/controllers/SlideController.js
+++ js/controllers/SlideController.js
@@ -4,7 +4,7 @@ tidal.controller('SlideController', ['$scope', function($scope) {
     {
       class: "carousel-item active slide-1",
       ref: "#one!",
-      image: 'images/Slides/Disk_Cover.png',
+      image: 'images/slides/Disk_Cover.png',
       imageClass: 'cover',
       title: "Action Bronson's Blue Chips 7000.",
       paragraph: "The long awaited Blue Chips sequel; Blue Chips 7000 is finally here.",
@@ -14,7 +14,7 @@ tidal.controller('SlideController', ['$scope', function($scope) {
     {
       class: "carousel-item slide-2",
       ref: "#two!",
-      image: 'images/Slides/Video_Screenshot.png',
+      image: 'images/slides/Video_Screenshot.png',
       imageClass: 'screenshot',
       title: "Watch exclusive videos.",
       paragraph: "Ut enim ad minim veniam, quis nostrud exercitaon ullamco laboris nisi ut aliquip ex ea.",
@@ -23,7 +23,7 @@ tidal.controller('SlideController', ['$scope', function($scope) {
     {
       class: "carousel-item slide-3",
       ref: "#three!",
-      image: 'images/Slides/Brush.png',
+      image: 'images/slides/Brush.png',
       imageClass: 'brush',
       title: "Experience Action Bronson live.",
       paragraph: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu.",
